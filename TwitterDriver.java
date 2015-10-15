07b17bf12a00c5eb4d0b6f608a83bf1021b35e8a
Gowtham <mggowtham25@gmail.com>
2015-10-15 15:37:51 -0700

    Update TwitterDriver.java

--- TwitterDriver.java
+++ TwitterDriver.java
@@ -1,97 +1,70 @@
-package FlumeProject;
 
-import java.io.IOException;
-import java.util.StringTokenizer;
+package twitter;
 
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 
-public class MR {
-	public static class Map extends Mapper<LongWritable,Text,Text,IntWritable>{
+public class TwitterDriver extends Configured implements Tool {
 
-		public void map(LongWritable key, Text value,
-				Context context)
-				throws IOException,InterruptedException {
-			
-			String line = value.toString();
-			StringTokenizer tokenizer = new StringTokenizer(line);
 
-			while (tokenizer.hasMoreTokens()) {
-				value.set(tokenizer.nextToken());
-				context.write(value, new IntWritable(1));
-			}
-	
-			
-		}
-		
-	}
-	public static class Reduce extends Reducer<Text,IntWritable,Text,IntWritable>{
-
-		public void reduce(Text key, Iterable<IntWritable> values,
-				Context context)
-				throws IOException,InterruptedException {
-			int sum=0;
-			// TODO Auto-generated method stub
-			for(IntWritable x: values)
-			{
-				sum+=x.get();
-			}
-			context.write(key, new IntWritable(sum));
-			
-		}
-		
-	}
-	
-	public static void main(String[] args) throws Exception {
+	@Override
+	public int run(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		//JobConf conf = new JobConf(WordCount.class);
-		Configuration conf= new Configuration();
+		Configuration conf = new Configuration();
 		
+		Job job = new Job(conf, "[Twitter Data Analyaser]");
 		
-		//conf.setJobName("mywc");
-		Job job = new Job(conf,"mywc");
+		job.setJarByClass(TwitterDriver.class);
+		job.setMapperClass(TwitterMapper.class);
+		job.setReducerClass(TwitterReducer.class);
 		
-		job.setJarByClass(MR.class);
-		job.setMapperClass(Map.class);
-		job.setReducerClass(Reduce.class);
-		
-		//conf.setMapperClass(Map.class);
-		//conf.setReducerClass(Reduce.class);
-
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(LongWritable.class);
 		
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 		
 		
+		MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class);
+		MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class);
+		//MultipleInputs.addInputPath(job, new Path(args[2]), TextInputFormat.class);
+		//MultipleInputs.addInputPath(job, new Path(args[3]), TextInputFormat.class);
 
-		Path outputPath = new Path(args[1]);
-			
-	        //Configuring the input/output path from the filesystem into the job
-	        
-	    FileInputFormat.addInputPath(job, new Path(args[0]));
-	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-			
-			//deleting the output path automatically from hdfs so that we don't have delete it explicitly
-			
-		outputPath.getFileSystem(conf).delete(outputPath);
-			
-			//exiting the job only if the flag value becomes false
-			
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	    FileOutputFormat.setOutputPath(job, new Path(args[2]));
+		
+		return job.waitForCompletion(true) ? 0 : 1;
+		
 	}
-}
 
+	public static void main(String[] args) {
+
+		try 
+		{
+		   TwitterDriver driver = new TwitterDriver();	
+           int successCode = ToolRunner.run(driver, args);
+		   System.exit(successCode); 	
+           
+		} 
+		catch (Exception e) 
+		{
+			// TODO: handle exception
+			System.out.println("Exception while running the Hadoop Job :" + e);
+
+			e.printStackTrace();
+		}
+
+	}
+
+}
